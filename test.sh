8db467ed331618bad981051011a3372ee9bf6e88
Dmitry <root@uralbash.ru>
2015-02-26 17:45:56 +0500

    Переписал тесты

--- test.sh
+++ test.sh
@@ -6,14 +6,25 @@
 # Distributed under terms of the MIT license.
 #
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+RST_FILES=`find . -name "*.rst" -printf "%p "`
+FLAKE8=$(flake8 ./sourcecode/)
+PEP8=$(pep8 ./sourcecode/)
+RST_CHECK=$(rstcheck $RST_FILES 3>&1 1>&2 2>&3 | tee >(cat - >&2)) # fd=STDERR_FILENO
+
 echo -e "${RED}"
-if rstcheck `find . -name "*.rst" -printf "%p "` ||
-    flake8 ./sourcecode/ ||
-    pep8 ./sourcecode/
+if [ -n "$RST_CHECK" ] ||
+    [ -n "$FLAKE8" ] ||
+    [ -n "$PEP8" ]
 then
+    echo -e "RST_CHECK: ${RST_CHECK:-OK}"
+    echo -e "PEP8: ${PEP8:-OK}"
+    echo -e "FLAKE8: ${FLAKE8:-OK}"
     exit 1
+else
+    echo -e "${GREEN}OK!"
 fi
 echo -e "${NC}"
 
