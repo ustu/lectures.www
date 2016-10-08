99f03c2a0628096dd882d6f9e50b0f46e7bebeac
Svintsov Dmitry <root@uralbash.ru>
2016-10-08 11:51:09 +0500

    Session->Python3

--- sourcecode/6.www.sync/2.codding/8.session/common.py
+++
@@ -1,8 +0,0 @@
-from beaker.session import Session
-
-
-def get_session(request={}, **kwargs):
-    """A shortcut for creating :class:`Session` instance"""
-    options = {}
-    options.update(**kwargs)
-    return Session(request, **options)
