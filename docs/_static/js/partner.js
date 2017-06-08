7c9d232cd646c58036168d0b27c8a8a1a7f76651
Svintsov Dmitry <root@uralbash.ru>
2017-06-08 15:08:51 +0500

    update theme

---
+++ docs/_static/js/partner.js
@@ -0,0 +1,18 @@
+// <!-- Yandex.RTB R-A-222712-1 -->
+(function(w, d, n, s, t) {
+  w[n] = w[n] || [];
+  w[n].push(function() {
+    Ya.Context.AdvManager.render({
+      blockId: "R-A-222712-1",
+      renderTo: "yandex-partner",
+      horizontalAlign: false,
+      async: true
+    });
+  });
+  t = d.getElementsByTagName("script")[0];
+  s = d.createElement("script");
+  s.type = "text/javascript";
+  s.src = "//an.yandex.ru/system/context.js";
+  s.async = true;
+  t.parentNode.insertBefore(s, t);
+})(window, window.document, "yandexContextAsyncCallbacks");
