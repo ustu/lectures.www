22ebd8dc761b84bcf9d12c62b37c55462663197d
Svintsov Dmitry <root@uralbash.ru>
2017-06-26 18:19:12 +0500

    test

--- docs/_static/js/partner.js
+++ docs/_static/js/partner.js
@@ -1,24 +1,40 @@
-var blockId = "";
-if (window.location.hostname == "lectures.uralbash.ru") {
-  blockId = "R-A-231461-1";
-} else {
-  blockId = "R-A-222712-1";
-}
-
-(function(w, d, n, s, t) {
-  w[n] = w[n] || [];
-  w[n].push(function() {
-    Ya.Context.AdvManager.render({
-      blockId: blockId,
-      renderTo: "yandex-partner",
-      horizontalAlign: false,
-      async: true
-    });
-  });
-  t = d.getElementsByTagName("script")[0];
-  s = d.createElement("script");
+(function(d) {
+  var t = d.getElementsByTagName("script")[0];
+  var s = d.createElement("script");
   s.type = "text/javascript";
-  s.src = "//an.yandex.ru/system/context.js";
+  s.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
   s.async = true;
   t.parentNode.insertBefore(s, t);
-})(window, window.document, "yandexContextAsyncCallbacks");
+
+  d.getElementById('yandex-partner').innerHTML = `
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-2884502571619359"
+     data-ad-slot="6348097736"></ins>
+`;
+})(window.document);
+(adsbygoogle = window.adsbygoogle || []).push({});
+// var blockId = "";
+// if (window.location.hostname == "lectures.uralbash.ru") {
+//   blockId = "R-A-231461-1";
+// } else {
+//   blockId = "R-A-222712-1";
+// }
+//
+// (function(w, d, n, s, t) {
+//   w[n] = w[n] || [];
+//   w[n].push(function() {
+//     Ya.Context.AdvManager.render({
+//       blockId: blockId,
+//       renderTo: "yandex-partner",
+//       horizontalAlign: false,
+//       async: true
+//     });
+//   });
+//   t = d.getElementsByTagName("script")[0];
+//   s = d.createElement("script");
+//   s.type = "text/javascript";
+//   s.src = "//an.yandex.ru/system/context.js";
+//   s.async = true;
+//   t.parentNode.insertBefore(s, t);
+// })(window, window.document, "yandexContextAsyncCallbacks");
