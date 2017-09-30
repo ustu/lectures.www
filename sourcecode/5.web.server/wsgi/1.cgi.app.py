977ddb6a2f175d7397e7c04ff719560e71edd8c7
Svintsov Dmitry <root@uralbash.ru>
2017-09-30 16:21:57 +0500

    fix wsgi slides

--- sourcecode/5.web.server/wsgi/1.cgi.app.py
+++ sourcecode/5.web.server/wsgi/1.cgi.app.py
@@ -21,10 +21,10 @@ def simple_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return ['Hello world!\n']
+    return 'Hello world!\n'
 
 
-class AppClass:
+class AppClass(object):
     def __init__(self, environ, start_response):
         self.environ = environ
         self.start = start_response
